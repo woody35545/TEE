3f432abb792b7181d6d0d91c05e6055846ae41b7
woody35545 <woody35545@gmail.com>
2022-04-11 20:34:51 +0900

    [+] main.c 백업으로 하나 복사

--- host/main_copy.c
+++ host/main_copy.c
@@ -1,55 +1,18 @@
-/*
- * Copyright (c) 2016, Linaro Limited
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <err.h>
 #include <stdio.h>
 #include <string.h>
-
-/* OP-TEE TEE client API (built by optee_client) */
 #include <tee_client_api.h>
-
-/* To the the UUID (found the the TA's h-file(s)) */
 #include <TEEencrypt_ta.h>
 
 char context_file_name[20];
 char option[10];
 char context_file_input_buffer[100];
 
+
+
 int main(int argc, char *argv[]) /* Arguemnt 받도록 변경하면 TEE 환경에서 TEEencrypt로 해당 바이너리 파일을 실행했을 때
 Argument 들이 잘 넘어오는지 확인해야함. */
 {
-	
-		
-
-
-
-
-	// 
-
 
 	TEEC_Result res;
 	TEEC_Context ctx;
@@ -75,89 +38,76 @@ Argument 들이 잘 넘어오는지 확인해야함. */
 		strcpy(context_file_name, argv[2]);
 	}
 
-	// Arguemnt 에 따라서 option 분류	
-	if (strcmp(option, "-e") == 0){
-		fs = fopen(context_file_name, "r");
-		fgets(context_file_input_buffer,sizeof(context_file_input_buffer,fs);
-		printf("[DEBUG] This is Encrypt Part\n");
-	}	 
-	if (strcmp(option, "-d") == 0){
-		fs = fopen(context_file_name, "r");
-		fgets(context_file_input_buffer,sizeof(context_file_input_buffer,fs);
-		printf("[DEBUG] This is Decrypt Part\n");	
-	}
+
+
 	
-
-
-
-	/* Initialize a context connecting us to the TEE */
 	res = TEEC_InitializeContext(NULL, &ctx);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
 
-	/*
-	 * Open a session to the "hello world" TA, the TA will print "hello
-	 * world!" in the log when the session is created.
-	 */
 	res = TEEC_OpenSession(&ctx, &sess, &uuid,
 			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
 	if (res != TEEC_SUCCESS)
 		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
 			res, err_origin);
 
-	/*
-	 * Execute a function in the TA by invoking it, in this case
-	 * we're incrementing a number.
-	 *
-	 * The value of command ID part and how the parameters are
-	 * interpreted is part of the interface provided by the TA.
-	 */
+	memset(&op, 0, sizeof(op)); // op 메모리 초기화
 
-	/* Clear the TEEC_Operation struct */
-	memset(&op, 0, sizeof(op));
-
-	/*
-	 * Prepare the argument. Pass a value in the first parameter,
-	 * the remaining three parameters are unused.
-	 */
-    op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE,
+	/* 파라미터 타입 설정, 첫번째 파라미터만 사용하고 나머지 3개는 사용안함. */
+    	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE,
 					 TEEC_NONE, TEEC_NONE);
 	//op.params[0].tmpref.buffer = plaintext;
 	op.params[0].tmpref.buffer = context_file_input_buffer;
 	op.params[0].tmpref.size = len;
-
-	printf("========================Encryption========================\n");
-	//printf("Please Input Plaintext : ");
-	//scanf("%[^\n]s",plaintext);
-
-	//memcpy(op.params[0].tmpref.buffer, plaintext, len);
-	memcpy(op.params[0].tmpref.buffer, context_file_input_buffer, len);
-
-
-	res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_ENCRYPT, &op,
-				 &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
-			res, err_origin);
-
-	memcpy(ciphertext, op.params[0].tmpref.buffer, len);
-	printf("Ciphertext : %s\n", ciphertext);
-
-	printf("========================Decryption========================\n");
-	//printf("Please Input Ciphertext : ");
-	//getchar();
-	//scanf("%[^\n]s",ciphertext);
-
-	//memcpy(op.params[0].tmpref.buffer, ciphertext, len);
-	memcpy(op.params[0].tmpref.buffer, context_file_input_buffer, len);
 	
-	res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_DECRYPT, &op,
+
+	/* 파라미터 세팅 완료 */
+
+
+	
+	// Arguemnt 에 따라서 option 분류	
+	if (strcmp(option, "-e") == 0){
+		fs = fopen(context_file_name, "r");
+		fgets(context_file_input_buffer,sizeof(context_file_input_buffer,fs);
+		printf("[DEBUG] This is Encrypt Part\n");
+		
+		/* Request TA to Encrypt */
+
+		printf("========================Encryption========================\n");
+	
+		memcpy(op.params[0].tmpref.buffer, context_file_input_buffer, len);
+
+		res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_ENCRYPT, &op, &err_origin);
+
+		if (res != TEEC_SUCCESS) errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x", res, err_origin);
+
+		memcpy(ciphertext, op.params[0].tmpref.buffer, len);
+
+		printf("Ciphertext : %s\n", ciphertext);
+	}	 
+
+
+	if (strcmp(option, "-d") == 0){
+		fs = fopen(context_file_name, "r");
+		fgets(context_file_input_buffer,sizeof(context_file_input_buffer,fs);
+		printf("[DEBUG] This is Decrypt Part\n");	
+	
+		
+		printf("========================Decryption========================\n");
+		memcpy(op.params[0].tmpref.buffer, context_file_input_buffer, len);
+	
+		res = TEEC_InvokeCommand(&sess, TA_TEEencrypt_CMD_DECRYPT, &op,
 				 &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
-			res, err_origin);
-	memcpy(plaintext, op.params[0].tmpref.buffer, len);
-	printf("Plaintext : %s\n", plaintext);
+		if (res != TEEC_SUCCESS) errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x", res, err_origin);
+		memcpy(plaintext, op.params[0].tmpref.buffer, len);
+		printf("Plaintext : %s\n", plaintext);
+	}
+	
+	// Option 분류 끝
+
+
+	// 세션 종료
+
 	TEEC_CloseSession(&sess);
 
 	TEEC_FinalizeContext(&ctx);
